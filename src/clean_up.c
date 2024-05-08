51321fc68bf7fc51e8d778d33a6a9dd9283e98a2
Hoang Tran <hoang.tran.fin@gmail.com>
2024-05-09 01:08:17 +0300

    Fixed free on unallocated memory

--- src/clean_up.c
+++ src/clean_up.c
@@ -6,7 +6,7 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 21:51:21 by hoatran           #+#    #+#             */
-/*   Updated: 2024/05/08 21:54:43 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/05/08 22:36:50 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	clean_up(t_minishell *minishell)
 {
-	ft_list_clear(&minishell->env_list, free);
+	ft_list_clear(&minishell->env_list, NULL);
 }
