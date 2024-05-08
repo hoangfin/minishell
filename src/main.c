ea48ea5f45e25e9e40b72e5f924121e03dcc1a3e
ito-miyuki <mitou.1221@gmail.com>
2024-05-08 15:07:41 +0300

    Added init_minishell()

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 16:31:20 by mito              #+#    #+#             */
-/*   Updated: 2024/05/07 17:47:40 by mito             ###   ########.fr       */
+/*   Updated: 2024/05/08 15:07:24 by mito             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 int main(int argc, char **argv, char **envp)
 {
-	char	buf[1024];
-	ssize_t	bytes_read;
+	t_minishell	minishell;
+	char		buf[1024];
+	ssize_t		bytes_read;
 
-	write(STDOUT_FILENO, "minishell > ", 12);
-	bytes_read = read(STDIN_FILENO, buf, 1024);
-	printf("Just behind first read\n");
-	while (bytes_read > 0)
-	{
-		//Do something here
-		write(STDOUT_FILENO, "minishell > ", 12);
-		bytes_read = read(STDIN_FILENO, buf, 1024);
-		printf("Just behind read in while loop\n");
-	}
+	if (init_minishell(&minishell, envp) < 0)
+		return (EXIT_FAILURE);
+	ft_env(&minishell);
+	// while (1)
+	// {
+	// 	//Do something here
+	// 	write(STDOUT_FILENO, "minishell > ", 12);
+	// 	bytes_read = read(STDIN_FILENO, buf, 1024);
+	// 	printf("Just behind read in while loop\n");
+	// }
 	return (EXIT_SUCCESS);
 }
