8cc69a58a807f083caf257d415488b19769e206c
Hoang Tran <hoang.tran.fin@gmail.com>
2024-05-09 01:11:27 +0300

    Initialize value to exit_status

--- src/initialization/init_minishell.c
+++ src/initialization/init_minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_minishell.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 14:36:12 by mito              #+#    #+#             */
-/*   Updated: 2024/05/08 15:05:42 by mito             ###   ########.fr       */
+/*   Updated: 2024/05/08 23:52:27 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static int	init_env_list(t_minishell *minishell, char **envp)
 {
 	t_node *node;
-	
+
 	minishell->env_list = ft_list(0);
 	if (minishell->env_list == NULL)
 		return (-1);
@@ -27,15 +27,16 @@ static int	init_env_list(t_minishell *minishell, char **envp)
 		ft_list_push(minishell->env_list, node);
 		envp++;
 	}
-	
+	return (0);
 }
 
 int	init_minishell(t_minishell *minishell, char **envp)
 {
 	if (init_env_list(minishell, envp) < 0)
 	{
-		// clean_up(minishell);
+		clean_up(minishell);
 		return (-1);
 	}
+	minishell->exit_status = 0;
 	return (0);
 }
