87ad8c32c214f1d562a025c3303e234d3e38cde1
Hoang Tran <hoatran@student.hive.fi>
2024-05-13 13:17:35 +0300

    Moved all header files .h to include directory

---
+++ include/command.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/12 17:18:49 by hoatran           #+#    #+#             */
+/*   Updated: 2024/05/13 01:31:12 by hoatran          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMMAND_H
+# define COMMAND_H
+
+# include "libft.h"
+
+typedef enum s_operator
+{
+	NO_OP,
+    OP_INPUT_REDIRECT,
+	OP_INPUT_HEREDOC,
+	OP_OUTPUT_REDIRECT,
+    OP_OUTPUT_APPEND,
+    OP_PIPE,
+    OP_AND,
+    OP_OR,
+}	t_operator;
+
+typedef struct s_io
+{
+	char		*name;
+	t_operator	operator;
+}	t_io;
+
+typedef struct s_command
+{
+	char	**argv;
+	t_list	*input_list;
+	t_list	*output_list;
+}	t_command;
+
+typedef struct s_command_line
+{
+	t_command	*cmds;
+	int			pipes_count;
+}	t_command_line;
+
+t_command	*parse_command(char *cmd_str);
+int			exe_cmd(t_command cmd);
+
+#endif
--- src/command/parse_cmd.c
+++ src/command/parse_cmd.c
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   add_signal_handler.c                               :+:      :+:    :+:   */
+/*   parse_cmd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/09 12:18:59 by hoatran           #+#    #+#             */
-/*   Updated: 2024/05/13 01:35:13 by hoatran          ###   ########.fr       */
+/*   Created: 2024/05/12 13:34:48 by hoatran           #+#    #+#             */
+/*   Updated: 2024/05/13 01:20:17 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell_signal.h"
+#include "command.h"
 
-int	add_signal_handler(int signum, sig_t handler)
+t_command	*parse_command(char *cmd_str)
 {
-	struct sigaction	sa;
+	t_command	*cmd;
 
-	sigemptyset(&sa.sa_mask);
-	sa.sa_handler = handler;
-	sa.sa_flags = 0;
-	return (sigaction(signum, &sa, NULL));
+	cmd = (t_command *)malloc(sizeof(t_command));
+	if (cmd == NULL)
+		return (NULL);
 }
