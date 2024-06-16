42d9166335a91c6033b9d4a8ea580819063bcc7b
Hoang Tran <hoang.tran.fin@gmail.com>
2024-06-16 22:57:11 +0300

    Fixed error message: unset HOME then export HOME;

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 15:02:39 by mito              #+#    #+#             */
-/*   Updated: 2024/06/11 17:25:44 by mito             ###   ########.fr       */
+/*   Updated: 2024/06/16 22:56:09 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,28 +39,34 @@ static int	update_pwd(const char *current_path, t_list *list)
 	return (0);
 }
 
-static int go_home(t_list *env_list)
+static int	go_home(t_list *env_list)
 {
-	const char	*home_path;
+	const char	*home_path = find_env("HOME", env_list);
 
-	home_path = find_env("HOME", env_list);
-	if (chdir(home_path) == -1)
+	if (home_path == NULL)
+	{
+		ft_fprintf(STDERR_FILENO, "minishell: cd: %s\n", "HOME not set");
 		return (-1);
+	}
+	if (chdir(home_path) == -1)
+	{
+		ft_fprintf(\
+			2, "minishell: cd: %s: %s\n", home_path, strerror(errno) \
+		);
+		return (-1);
+	}
 	update_pwd(home_path, env_list);
 	return (0);
 }
-int ft_cd(t_command *cmd, t_list *env_list, t_list *export_list)
+
+int	ft_cd(t_command *cmd, t_list *env_list, t_list *export_list)
 {
 	char	buffer[1024];
 
-    if (cmd->argv[1] == NULL)
+	if (cmd->argv[1] == NULL)
 	{
-        if (go_home(env_list) < 0)
-		{
-			ft_fprintf(STDERR_FILENO, "minishell: %s: %s\n",
-				cmd->argv[0], "HOME not set");
+		if (go_home(env_list) < 0)
 			return (1);
-		}
 		return (0);
 	}
 	if (cmd->argv[1][0] == '\0')
@@ -75,6 +81,5 @@ int ft_cd(t_command *cmd, t_list *env_list, t_list *export_list)
 		return (1);
 	update_pwd(buffer, env_list);
 	update_pwd(buffer, export_list);
-    return (0);
+	return (0);
 }
-
